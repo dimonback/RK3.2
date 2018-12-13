11304b1f2064d869b2ac1b4781246629e205d4ae
Дима Сорокин <sorokin.00@mail.ru>
2018-12-13 10:54:22 +0300

    Initial Commit

---
+++ RK3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  RK3
+//
+//  Created by Дмитрий Сорокин on 13/12/2018.
+//  Copyright © 2018 Dimon_back. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
